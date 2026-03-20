db40fa47e19175218ac443a8fbd3c3417ff1a11f
Rembold <matheus.rembold@gmail.com>
2026-03-19 23:14:35 -0300

    entrega Final

--- consultas.sql
+++ consultas.sql
@@ -78,47 +78,49 @@ GROUP BY tipo_compra;
 
 -- Dia da semana com mais transações ou maior volume
 
-
-SELECT 
-    CASE 
-        WHEN f.total_parcelas = 1 THEN 'À vista'
-        ELSE 'Parcelado'
-    END AS tipo_compra,
-    COUNT(*) AS quantidade,
-    SUM(f.valor_brl) AS total_gasto
-FROM fato_transacao f
-GROUP BY tipo_compra;
-
 SELECT 
     d.dia_semana,
-    COUNT(*) AS qtd_transacoes
+    COUNT(*) AS qtd_transacoes,
+    SUM(f.valor_brl) AS total_gasto,
+    AVG(f.valor_brl) AS ticket_medio
 FROM fato_transacao f
 JOIN dim_data d ON f.id_data = d.id_data
 GROUP BY d.dia_semana
-ORDER BY qtd_transacoes DESC;
+ORDER BY total_gasto DESC;
 
 
 -- Estornos e créditos: total e impacto por titular/categoria
 
+-- Visao geral
 SELECT 
-    SUM(f.valor_brl) AS total_estornos
+    COUNT(*) AS qtd_estornos,
+    SUM(f.valor_brl) AS total_estornado,
+    AVG(f.valor_brl) AS media_estorno
 FROM fato_transacao f
 WHERE f.valor_brl < 0;
 
+-- impacto por titular
+
 SELECT 
     t."Nome no Cartão",
-    SUM(f.valor_brl) AS total_estornos
+    COUNT(*) AS qtd_estornos,
+    SUM(f.valor_brl) AS total_estornado,
+    AVG(f.valor_brl) AS media_estorno
 FROM fato_transacao f
 JOIN dim_titular t ON f.id_titular = t.id_titular
 WHERE f.valor_brl < 0
 GROUP BY t."Nome no Cartão"
-ORDER BY total_estornos;
+ORDER BY total_estornado ASC;
+
+-- impacto por categoria 
 
 SELECT 
     c."Categoria",
-    SUM(f.valor_brl) AS total_estornos
+    COUNT(*) AS qtd_estornos,
+    SUM(f.valor_brl) AS total_estornado,
+    AVG(f.valor_brl) AS media_estorno
 FROM fato_transacao f
 JOIN dim_categoria c ON f.id_categoria = c.id_categoria
 WHERE f.valor_brl < 0
 GROUP BY c."Categoria"
-ORDER BY total_estornos;
+ORDER BY total_estornado ASC; 
